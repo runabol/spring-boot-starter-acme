46d633130cf0fc81c03c9a23b206857397c01bda
Cohen, Arik <arik.cohen@smithsonianchannel.com>
2018-02-05 19:47:29 -0500

    first commit

---
+++ src/main/java/com/creactiviti/spring/boot/starter/letsencrypt/SpringBootStarterLetsencryptApplication.java
@@ -0,0 +1,12 @@
+package com.creactiviti.spring.boot.starter.letsencrypt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterLetsencryptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterLetsencryptApplication.class, args);
+	}
+}
