b8b0cdb38ffdc5c998dd94766fda92e5193ecdf6
Cohen, Arik <arik.cohen@smithsonianchannel.com>
2018-02-07 19:34:26 -0500

    cleanup

--- src/main/java/com/creactiviti/spring/boot/starter/letsencrypt/SpringBootStarterLetsencryptApplication.java
+++ src/main/java/com/creactiviti/spring/boot/starter/letsencrypt/SpringBootStarterLetsencryptApplication.java
@@ -1,12 +1,23 @@
 package com.creactiviti.spring.boot.starter.letsencrypt;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootStarterLetsencryptApplication {
+public class SpringBootStarterLetsencryptApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStarterLetsencryptApplication.class, args);
 	}
+	
+	
+
+  @Override
+  public void run(String... args) throws Exception {
+    
+      
+  }
+
+	
 }
